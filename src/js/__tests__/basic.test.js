a117d623090929a8ffa2616330526ef147ea4e47
laalipop <preicpodhya@mail.ru>
2021-02-05 16:40:15 +0300

    two files changed

--- src/js/__tests__/basic.test.js
+++ src/js/__tests__/basic.test.js
@@ -1,26 +1,26 @@
 import orderByProps from '../basic';
 
-// test('normal sort', () => {
-//   const result = [
-//     { key: 'level', value: 2 },
-//     { key: 'name', value: 'мечник' },
-//     { key: 'attack', value: 80 },
-//     { key: 'defence', value: 40 },
-//     { key: 'health', value: 10 },
-//   ];
+test('normal sort', () => {
+  const result = [
+    { key: 'level', value: 2 },
+    { key: 'name', value: 'мечник' },
+    { key: 'attack', value: 80 },
+    { key: 'defence', value: 40 },
+    { key: 'health', value: 10 },
+  ];
 
-//   const test = orderByProps({
-//     name: 'мечник',
-//     health: 10,
-//     level: 2,
-//     attack: 80,
-//     defence: 40,
-//   }, ['level', 'name']);
+  const test = orderByProps({
+    name: 'мечник',
+    health: 10,
+    level: 2,
+    attack: 80,
+    defence: 40,
+  }, ['level', 'name']);
 
-//   expect(test).toEqual(result);
-// });
+  expect(test).toEqual(result);
+});
 
-test('no normal sort', () => {
+test('no normal sort / without an array to sort', () => {
   const result = [
     { key: 'attack', value: 80 },
     { key: 'defence', value: 40 },
@@ -35,7 +35,27 @@ test('no normal sort', () => {
     level: 2,
     attack: 80,
     defence: 40,
-  });
+  }, []);
+
+  expect(test).toEqual(result);
+});
+
+test('no normal sort / with a missing key', () => {
+  const result = [
+    { key: 'attack', value: 80 },
+    { key: 'defence', value: 40 },
+    { key: 'health', value: 10 },
+    { key: 'level', value: 2 },
+    { key: 'name', value: 'мечник' },
+  ];
+
+  const test = orderByProps({
+    name: 'мечник',
+    health: 10,
+    level: 2,
+    attack: 80,
+    defence: 40,
+  }, ['wow']);
 
   expect(test).toEqual(result);
 });
--- src/js/basic.js
+++ src/js/basic.js
@@ -1,28 +1,31 @@
-const resultArr = [];
-
-function sortArr(obj, arr) {
-  for (const prop in arr) {
-    if (arr[prop] in obj) {
-      resultArr.push({ key: arr[prop], value: obj[arr[prop]] });
-      delete obj[arr[prop]];
+function sortArr(inputObj, namesPropsArray) {
+  const resultArr = [];
+  for (const prop in namesPropsArray) {
+    if (namesPropsArray[prop] in inputObj) {
+      const keyProp = namesPropsArray[prop];
+      resultArr.push({ key: keyProp, value: inputObj[keyProp] });
     }
   }
-}
-
-export default function orderByProps(obj, arr) {
-  sortArr(obj, arr);
-  const sortt = Object.keys(obj).sort();
-  sortArr(obj, sortt);
-  // console.log(resultArr);
   return resultArr;
 }
 
-// const obj = {
-//   name: 'мечник',
-//   health: 10,
-//   level: 2,
-//   attack: 80,
-//   defence: 40,
-// };
+function getNeedSortingFields(inputObj, namesPropsArray) {
+  const copyObj = { ...inputObj };
+  for (const prop in namesPropsArray) {
+    if (namesPropsArray[prop] in inputObj) {
+      delete copyObj[namesPropsArray[prop]];
+    }
+  }
+  return copyObj;
+}
 
-// orderByProps(obj, []);
+export default function orderByProps(obj, arr) {
+  const resultFirst = sortArr(obj, arr);
+  const copyObj = getNeedSortingFields(obj, arr);
+  const sortt = Object.keys(copyObj).sort();
+
+  const resultSecond = sortArr(obj, sortt);
+  const resultArray = resultFirst.concat(resultSecond);
+
+  return resultArray;
+}
